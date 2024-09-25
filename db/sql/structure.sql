c7241a7e0c575049ff0279a832a2dbbcea161359
Thomas T <huulbaek@gmail.com>
2024-09-25 10:02:26 +0200

    Add measurement_point_number and remove station_uid

--- db/sql/structure.sql
+++ db/sql/structure.sql
@@ -4,8 +4,7 @@ SET search_path TO hydrometry, public;
 
 CREATE TABLE station
 (
- station_uid        uuid NOT NULL,
- station_id         char(8) NULL,
+ station_id         char(8) NOT NULL,
  old_station_number char(8) NULL,
  name               varchar(150) NULL,
  station_owner_name varchar(150) NULL,
@@ -13,17 +12,13 @@ CREATE TABLE station
  description        varchar(200) NULL,
  created            timestamp with time zone NOT NULL,
  updated            timestamp with time zone NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( station_uid )
-);
-CREATE INDEX station_IDX_station ON station (
-    station_id
+ CONSTRAINT PK_1 PRIMARY KEY ( station_id )
 );
 CREATE INDEX station_IDX_location ON station USING GIST (
-    location
+  location
 );
 
 COMMENT ON TABLE station IS 'Contains information about the station';
-COMMENT ON COLUMN station.station_uid IS 'The unique identifier of the station, identical with the source (DMP)';
 COMMENT ON COLUMN station.station_id IS 'ID of the station, 8 characters';
 COMMENT ON COLUMN station.old_station_number IS 'Old station number, 8 characters';
 COMMENT ON COLUMN station.name IS 'Name of the station';
@@ -33,13 +28,13 @@ COMMENT ON COLUMN station.description IS 'Description of the station that can co
 
 CREATE TABLE measurement_type
 (
- measurement_type_id  int NOT NULL,
- unit                 varchar(4) NOT NULL,
- unit_sc              int NOT NULL,
- "parameter"          varchar(10) NOT NULL,
- parameter_sc         int NOT NULL,
- examination_type_sc  int NOT NULL,
- examination_type     varchar(10) NOT NULL,
+ measurement_type_id      int NOT NULL,
+ unit                     varchar(4) NOT NULL,
+ unit_sc                  int NOT NULL,
+ "parameter"              varchar(10) NOT NULL,
+ parameter_sc             int NOT NULL,
+ examination_type_sc      int NOT NULL,
+ examination_type         varchar(10) NOT NULL,
  CONSTRAINT PK_3 PRIMARY KEY ( measurement_type_id )
 );
 
@@ -51,6 +46,7 @@ COMMENT ON COLUMN measurement_type.parameter IS 'The parameter of the measuremen
 COMMENT ON COLUMN measurement_type.parameter_sc IS 'The stancode of the parameter';
 COMMENT ON COLUMN measurement_type.examination_type_sc IS 'The stancode of the examination type';
 COMMENT ON COLUMN measurement_type.examination_type IS 'The name of the examination type, for example "Vandføring" or "Vandstand"';
+COMMENT ON COLUMN measurement_type.examination_type IS 'The name of the examination type, for example "Vandføring" or "Vandstand"';
 
 CREATE TABLE measurement
 (
@@ -58,10 +54,11 @@ CREATE TABLE measurement
  measurement_date_time timestamp with time zone NOT NULL,
  is_current            bool NOT NULL,
  created               timestamp with time zone NOT NULL,
- station_uid           uuid NOT NULL,
+ station_id            char(8) NOT NULL,
  measurement_type_id   int NOT NULL,
+ measurement_point_number int NOT NULL,
  CONSTRAINT FK_1 FOREIGN KEY ( measurement_type_id ) REFERENCES measurement_type ( measurement_type_id ),
- CONSTRAINT FK_2 FOREIGN KEY ( station_uid ) REFERENCES station ( station_uid )
+ CONSTRAINT FK_2 FOREIGN KEY ( station_id ) REFERENCES station ( station_id )
 );
 
 CREATE INDEX FK_1_M ON measurement
@@ -70,11 +67,11 @@ CREATE INDEX FK_1_M ON measurement
 );
 CREATE INDEX FK_2_M ON measurement
 (
- station_uid
+ station_id
 );
 CREATE UNIQUE INDEX measurement_station_IDX ON measurement
 (
-  station_uid,
+  station_id,
   measurement_type_id,
   is_current,
   measurement_date_time
@@ -92,15 +89,15 @@ CREATE TABLE calculated
  mean                 real NOT NULL,
  created              timestamp with time zone NOT NULL,
  updated              timestamp with time zone NOT NULL,
- station_uid          uuid NOT NULL,
+ station_id           char(8) NOT NULL,
  measurement_type_id  int NOT NULL,
- CONSTRAINT FK_3 FOREIGN KEY ( station_uid ) REFERENCES station ( station_uid ),
+ CONSTRAINT FK_3 FOREIGN KEY ( station_id ) REFERENCES station ( station_id ),
  CONSTRAINT FK_4 FOREIGN KEY ( measurement_type_id ) REFERENCES measurement_type ( measurement_type_id )
 );
 
 CREATE INDEX FK_1_C ON calculated
 (
- station_uid
+ station_id
 );
 CREATE INDEX FK_2_C ON calculated
 (
@@ -110,7 +107,7 @@ CREATE INDEX FK_2_C ON calculated
 CREATE UNIQUE INDEX calculated_IDX ON calculated
 (
  slice,
- station_uid,
+ station_id,
  date_start,
  measurement_type_id
 );
