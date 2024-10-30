22d906b16b8189936565394b6adeff90c61ec302
Radu Dudici <rd@danterm.dk>
2024-10-30 08:42:16 +0100

    Update SQL for measurement

--- src/main/java/dk/dataforsyningen/vanda_hydrometry_data/dao/MeasurementDao.java
+++ src/main/java/dk/dataforsyningen/vanda_hydrometry_data/dao/MeasurementDao.java
@@ -79,23 +79,6 @@ public interface MeasurementDao {
 			""")
 	Measurement addMeasurement(@BindBean Measurement measurement);
 	
-	/**
-	 * Update measurement result if it exists
-	 * 
-	 * @param measurement
-	 */
-	@SqlUpdate("""
-			update hydrometry.measurement set result = :result, result_elevation_corrected = :resultElevationCorrected, updated = now()
-			where
-				station_id = :stationId
-				and measurement_date_time = :measurementDateTime
-				and measurement_point_number = :measurementPointNumber
-				and examination_type_sc = :examinationTypeSc
-				and (result != :result or result_elevation_corrected != :resultElevationCorrected)
-				and is_current = :isCurrent
-			""")
-	void updateMeasurement(@BindBean Measurement measurement);
-	
 	@SqlBatch("""
 			insert into hydrometry.measurement (station_id, measurement_date_time, vanda_event_timestamp, measurement_point_number, examination_type_sc, result, result_elevation_corrected, is_current, created, updated)
 			(select :stationId, :measurementDateTime, :vandaEventTimestamp, :measurementPointNumber, :examinationTypeSc, :result, :resultElevationCorrected, :isCurrent, now(), now()
@@ -109,7 +92,23 @@ public interface MeasurementDao {
 			))
 			""")
 	void addMeasurements(@BindBean List<Measurement> measurements);
-	
+
+	/**
+	 * Update measurement result if it exists
+	 * 
+	 * @param measurement
+	 */
+	@SqlUpdate("""
+			update hydrometry.measurement set result = :result, result_elevation_corrected = :resultElevationCorrected, updated = now()
+			where
+				station_id = :stationId
+				and measurement_date_time = :measurementDateTime
+				and measurement_point_number = :measurementPointNumber
+				and examination_type_sc = :examinationTypeSc
+				and is_current = :isCurrent
+			""")
+	void updateMeasurement(@BindBean Measurement measurement);
+		
 	@SqlBatch("""
 			update hydrometry.measurement set result = :result, result_elevation_corrected = :resultElevationCorrected, updated = now()
 			where
@@ -117,7 +116,6 @@ public interface MeasurementDao {
 				and measurement_date_time = :measurementDateTime
 				and measurement_point_number = :measurementPointNumber
 				and examination_type_sc = :examinationTypeSc
-				and (result != :result or result_elevation_corrected != :resultElevationCorrected)
 				and is_current = :isCurrent
 			""")
 	void updateMeasurements(@BindBean List<Measurement> measurements);
