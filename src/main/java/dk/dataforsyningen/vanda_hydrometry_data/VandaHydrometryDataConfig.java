c1b58e8cb8c9da6da4991083abc92feb7de126fe
seskildsen <sines@sdfi.dk>
2024-12-02 16:08:20 +0100

    refactor: use OffsetDateTime.parse directly
    Let the code fail hard if the client does not give correct valid datetime and not in UTC (Zulu)

--- src/main/java/dk/dataforsyningen/vanda_hydrometry_data/VandaHydrometryDataConfig.java
+++ src/main/java/dk/dataforsyningen/vanda_hydrometry_data/VandaHydrometryDataConfig.java
@@ -1,21 +1,8 @@
 package dk.dataforsyningen.vanda_hydrometry_data;
 
 import java.security.InvalidParameterException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
-import java.time.format.SignStyle;
-import java.time.temporal.ChronoField;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,29 +15,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class VandaHydrometryDataConfig {
 
-  private static final Logger log = LoggerFactory.getLogger(VandaHydrometryDataConfig.class);
-
-  private static final DateTimeFormatter FLEXIBLE_FORMATTER =
-	        new DateTimeFormatterBuilder()
-	            .append(DateTimeFormatter.ISO_LOCAL_DATE) // Date part
-	            .optionalStart()
-	            .appendLiteral('T')
-	            .appendValue(ChronoField.HOUR_OF_DAY, 1, 2, SignStyle.NOT_NEGATIVE) // 1-2 digits for hours
-	            .optionalStart()
-	            .appendLiteral(':')
-	            .appendValue(ChronoField.MINUTE_OF_HOUR, 1, 2, SignStyle.NOT_NEGATIVE) // 1-2 digits for minutes
-	            .optionalStart()
-	            .appendLiteral(':')
-	            .appendValue(ChronoField.SECOND_OF_MINUTE, 1, 2, SignStyle.NOT_NEGATIVE) // 1-2 digits for seconds
-	            .optionalStart()
-	            .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true) // Fractional seconds optional
-	            .optionalEnd()
-	            .optionalEnd()
-	            .optionalEnd()
-	            .optionalEnd()
-	            .appendOffsetId() // Time zone is mandatory if present
-	            .toFormatter();
-  
   /* Values from the properties file */
   //enables DAO and database service testing - needs a DB connection
   @Value("${vanda-hydrometry-data.database.test:#{false}}")
@@ -121,9 +85,9 @@ public class VandaHydrometryDataConfig {
   }
 
   public Integer getExaminationTypeSc() {
-      if (examinationTypeSc == null) {
-          return null;
-      }
+    if (examinationTypeSc == null) {
+        return null;
+    }
     try {
       return Integer.parseInt(examinationTypeSc);
     } catch (NumberFormatException ex) {
@@ -132,9 +96,9 @@ public class VandaHydrometryDataConfig {
   }
 
   public Integer getParameterSc() {
-      if (parameterSc == null) {
-          return null;
-      }
+    if (parameterSc == null) {
+        return null;
+    }
     try {
       return Integer.parseInt(parameterSc);
     } catch (NumberFormatException ex) {
@@ -143,9 +107,9 @@ public class VandaHydrometryDataConfig {
   }
 
   public Integer getMeasurementPointNumber() {
-      if (measurementPointNumber == null) {
-          return null;
-      }
+    if (measurementPointNumber == null) {
+        return null;
+    }
     try {
       return Integer.parseInt(measurementPointNumber);
     } catch (NumberFormatException ex) {
@@ -154,62 +118,64 @@ public class VandaHydrometryDataConfig {
   }
 
   public OffsetDateTime getWithResultsAfter() {
-      if (withResultsAfter == null) {
-          return null;
-      }
+    if (withResultsAfter == null) {
+        return null;
+    }
     try {
-      return parseForAPI(withResultsAfter, true, true);
-    } catch (DateTimeParseException | NullPointerException ex) {
+      return OffsetDateTime.parse(withResultsAfter);
+    } catch (DateTimeParseException | NullPointerException exception) {
       throw new InvalidParameterException(
-          "Invalid date format found in 'withResultsAfter' parameter.");
+          "Invalid date format found in 'withResultsAfter' parameter: " + exception.getMessage() + ". Remember that it must be defined without second component as an UTC timestamp in the RFC 3339 date+time format. For example '2023-09-21T14:34Z'.");
     }
   }
 
   public OffsetDateTime getWithResultsCreatedAfter() {
-      if (withResultsCreatedAfter == null) {
-          return null;
-      }
+    if (withResultsCreatedAfter == null) {
+        return null;
+    }
     try {
-      return parseForAPI(withResultsCreatedAfter, true, true);
-    } catch (DateTimeParseException | NullPointerException ex) {
+      return OffsetDateTime.parse(withResultsCreatedAfter);
+    } catch (DateTimeParseException | NullPointerException exception) {
       throw new InvalidParameterException(
-          "Invalid date format found in 'withResultsCreatedAfter' parameter.");
+          "Invalid date format found in 'withResultsCreatedAfter' parameter: " + exception.getMessage() + ". Remember that it must be defined without second component as an UTC timestamp in the RFC 3339 date+time format. For example '2023-09-21T14:34Z'.");
     }
   }
 
   public OffsetDateTime getFrom() {
-      if (from == null) {
-          return null;
-      }
+    if (from == null) {
+        return null;
+    }
     try {
-      return parseForAPI(from, true, true);
-    } catch (DateTimeParseException | NullPointerException ex) {
-      throw new InvalidParameterException("Invalid date format found in 'from' parameter.");
+      return OffsetDateTime.parse(from);
+    } catch (DateTimeParseException | NullPointerException exception) {
+      throw new InvalidParameterException(
+          "Invalid date format found in 'from' parameter: " + exception.getMessage() + ". Remember that it must be defined without second component as an UTC timestamp in the RFC 3339 date+time format. For example '2023-09-21T14:34Z'.");
     }
   }
 
   public OffsetDateTime getTo() {
-      if (to == null) {
-          return null;
-      }
+    if (to == null) {
+        return null;
+    }
     try {
-      return parseForAPI(to, true, true);
-    } catch (DateTimeParseException | NullPointerException ex) {
-      throw new InvalidParameterException("Invalid date format found in 'to' parameter.");
+      return OffsetDateTime.parse(to);
+    } catch (DateTimeParseException | NullPointerException exception) {
+      throw new InvalidParameterException(
+          "Invalid date format found in 'to' parameter: " + exception.getMessage() + ". Remember that it must be defined without second component as an UTC timestamp in the RFC 3339 date+time format. For example '2023-09-21T14:34Z'.");
     }
   }
 
   public OffsetDateTime getCreatedAfter() {
-      if (createdAfter == null) {
-          return null;
-      }
+    if (createdAfter == null) {
+        return null;
+    }
     try {
-      return parseForAPI(createdAfter, true, true);
-    } catch (DateTimeParseException | NullPointerException ex) {
-      throw new InvalidParameterException("Invalid date format found in 'createdAfter' parameter.");
+      return OffsetDateTime.parse(createdAfter);
+    } catch (DateTimeParseException | NullPointerException exception) {
+      throw new InvalidParameterException(
+          "Invalid date format found in 'to' parameter: " + exception.getMessage() + ". Remember that it must be defined without second component as an UTC timestamp in the RFC 3339 date+time format. For example '2023-09-21T14:34Z'.");
     }
   }
-
 
   @Override
   public String toString() {
@@ -338,67 +304,4 @@ public class VandaHydrometryDataConfig {
   public boolean isEnableTest() {
     return enableTest;
   }
-
-  /**
-   * Parses the date/time string given by the user into an OffsetDateTime.
-   * If required the output can be in UTC time zone with seconds/nano removed as accepted by the API.
-   *
-   * @param string date/time with or without TZ
-   * @param boolean inUTC if true the result will be converted and returned in UTC no matter the input TZ
-   * @param boolean withoutSeconds if true the seconds and milliseconds will be set to 0
-   * @return OffsetDateTime
-   */
-  public static OffsetDateTime parseForAPI(String dateStr, boolean inUTC, boolean withoutSeconds) throws DateTimeParseException {
-    if (dateStr == null) {
-      return null;
-    }
-    dateStr = dateStr.toUpperCase();
-    OffsetDateTime output = null;
-    try {
-        // Attempt to parse with ISO_OFFSET_DATE_TIME format (includes time zone)
-        output = OffsetDateTime.parse(dateStr, FLEXIBLE_FORMATTER);
-    } catch (DateTimeParseException e) {
-        try {
-        	
-        	// Handle cases where only date and time zone are present (e.g., "2023-11-28+01:00")
-        	Matcher matcher = Pattern.compile("^(\\d{4}-\\d{2}-\\d{2})(Z|[+-]\\d{2}:\\d{2})$").matcher(dateStr);
-            if (matcher.matches()) {
-                String datePart = matcher.group(1);
-                String zonePart = matcher.group(2);
-                output = OffsetDateTime.parse(datePart + "T00:00:00" + zonePart, FLEXIBLE_FORMATTER);
-                
-            } else {
-	            // Attempt to parse as LocalDateTime (no time zone provided)
-	            LocalDateTime localDateTime;
-	            if (dateStr.contains("T")) {
-	                // If the time component is present
-	                localDateTime = LocalDateTime.parse(dateStr, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-	            } else {
-	                // If only the date is provided, assume 00:00:00
-	                LocalDate localDate = LocalDate.parse(dateStr, DateTimeFormatter.ISO_LOCAL_DATE);
-	                localDateTime = localDate.atStartOfDay();
-	            }
-	            // Apply the system's default time zone
-	            ZoneId systemZone = ZoneId.systemDefault();
-	            ZoneOffset systemOffset = systemZone.getRules().getOffset(localDateTime);
-	            output = localDateTime.atOffset(systemOffset);
-            }
-        } catch (DateTimeParseException e2) {
-            throw new InvalidParameterException("Invalid date/time format. Expected ISO-8601 format, with or without time zone.", e2);
-        }
-    }
-    
-    if (inUTC && !ZoneOffset.UTC.equals(output.getOffset())) {
-    	output = output.withOffsetSameInstant(ZoneOffset.UTC);
-    }
-    
-    if (withoutSeconds) {
-    	output = output
-    			.with(ChronoField.SECOND_OF_MINUTE, 0)  // Set seconds to 0
-                .with(ChronoField.NANO_OF_SECOND, 0);   // Set nanoseconds to 0
-    }
-    
-    return output;
-  }
-
 }
