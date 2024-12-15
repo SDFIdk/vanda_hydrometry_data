c846526ffe3ab4c87e83e0e33d846d8ccc336042
seskildsen <sines@sdfi.dk>
2024-12-15 13:11:09 +0100

    fix: speed up SQL and only want to change is_current is true
    It takes a lot of more time to also change rows that already have is_current=false

--- src/main/java/dk/dataforsyningen/vanda_hydrometry_data/dao/MeasurementDao.java
+++ src/main/java/dk/dataforsyningen/vanda_hydrometry_data/dao/MeasurementDao.java
@@ -100,6 +100,7 @@ public interface MeasurementDao {
       	and measurement_date_time = :measurementDateTime
       	and measurement_point_number = :measurementPointNumber
       	and examination_type_sc = :examinationTypeSc
+      	and is_current = true
       """)
   int inactivateMeasurementHistory(@BindBean Measurement measurement);
 
@@ -116,6 +117,7 @@ public interface MeasurementDao {
       	and measurement_date_time = :measurementDateTime
       	and measurement_point_number = :measurementPointNumber
       	and examination_type_sc = :examinationTypeSc
+      	and is_current = true
       """)
   void inactivateMeasurementsHistory(@BindBean List<Measurement> measurements);
 
